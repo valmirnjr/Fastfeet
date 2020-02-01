1db4b100366657f294f7f6edc825217ae5c79f82
Valmir Jr <valmir.junior@ee.ufcg.edu.br>
2020-02-01 19:56:36 -0300

    chore: first commit

---
+++ backend/src/app/models/DeliveryMan.js
@@ -0,0 +1,19 @@
+import Sequelize, { Model } from "sequelize";
+
+class DeliveryMan extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+}
+
+export default DeliveryMan;
