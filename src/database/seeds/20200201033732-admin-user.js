eaa26acafca46c5274ea3a6258dce6a72db6d4e2
Valmir Jr <valmir.junior@ee.ufcg.edu.br>
2020-02-01 00:51:23 -0300

    fix: database config path fixed

---
+++ src/database/seeds/20200201033732-admin-user.js
@@ -0,0 +1,21 @@
+const bcrypt = require("bcryptjs");
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          name: "Distribuidora Fastfeed",
+          email: "admin@fastfeed.com",
+          password_hash: bcrypt.hashSync("123456", 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: () => {},
+};
