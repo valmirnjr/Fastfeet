9c19295e3f8ec9ea416e92eeee553c2c1f589a0a
Valmir Jr <valmir.junior@ee.ufcg.edu.br>
2020-02-01 22:08:12 -0300

    chore: first commit

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
+import multer from "multer";
+import multerConfig from "./config/multer";
 
 import SessionController from "./app/controllers/SessionController";
 import RecipientController from "./app/controllers/RecipientController";
 import DeliveryManController from "./app/controllers/DeliveryManController";
+import FileController from "./app/controllers/FileController";
 
 import authMiddleware from "./app/middlewares/auth";
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post("/users", SessionController.store);
 
@@ -19,5 +23,7 @@ routes.get("/deliverymen", DeliveryManController.index);
 routes.post("/deliverymen", DeliveryManController.store);
 routes.put("/deliverymen/:id", DeliveryManController.update);
 routes.delete("/deliverymen/:id", DeliveryManController.delete);
+
+routes.post("/files", upload.single("file"), FileController.store);
 
 export default routes;
